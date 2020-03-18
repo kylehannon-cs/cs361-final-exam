5cf3d6afb561443176693326b4fc02d23f64bfa5
Kyle Hannon <khannon@cocc.edu>
2020-03-18 11:36:20 -0700

    Fixing dependency w/ injections #1

--- main.rb
+++ main.rb
@@ -6,23 +6,28 @@
 require_relative 'bmx_bike'
 require_relative 'road_bike'
 require_relative 'mountain_bike'
+require_relative 'tail_pack.rb'
+require_relative 'roll_pack.rb'
+require_relative 'pannier.rb'
+
+tailPack.new
 
 bikes = [
-  BmxBike.new,
+  BmxBike(tailPack).new,
   RoadBike.new,
   MountainBike.new,
   MountainBike.new
 ]
 
 total_price = 0
-total_price += bikes[0].weekly_rate
+total_price += bikes[0].daily_rate * 7
 total_price += bikes[0].tail_pack.cost
 total_price += bikes[1].daily_rate * 7
 total_price += bikes[1].panniers[0].price
 total_price += bikes[1].panniers[1].price
-total_price += bikes[2].weekly_rate
+total_price += bikes[2].daily_rate * 7
 total_price += bikes[2].luggage.price
-total_price += bikes[3].weekly_rate
+total_price += bikes[3].daily_rate * 7
 total_price += bikes[3].luggage.price
 
 puts "Total price: #{total_price}"
